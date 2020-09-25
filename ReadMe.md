0b670284e29a307aa6074a81ddbf8fdf22888b20
Robert Haines <rhaines@manchester.ac.uk>
2020-09-25 15:34:20 +0100

    Initial commit.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# COMP62521 Lab Source
+
+## Robert Haines, Gerard Capes, Peter Crowther and the members of Team Beedle (2013)
+
+This is the initial lab source code for COMP62521.
+
+The original source was written by Team Beedle (2013). Conversion to a generic start point for subsequent labs was by Robert Haines. Conversion to Python3 and general tidy-up by Gerard Capes and Peter Crowther.
+
+Please see Blackboard for instructions on how to get started.
---
+++ test/test_app.py
@@ -0,0 +1,22 @@
+from os import path
+import unittest
+import comp62521
+
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        directory, _ = path.split(__file__)
+        data = "dblp_curated_sample.xml"
+        comp62521.app.config['TESTING'] = True
+        comp62521.app.config['DATASET'] = data
+        comp62521.app.config['DATABASE'] = path.join(directory, "..", "data", data)
+        self.app = comp62521.app.test_client()
+
+    def test_home(self):
+        r = self.app.get("/")
+        self.assertEqual(200, r.status_code, "Status code was not 'OK'.")
+
+
+if __name__ == '__main__':
+    unittest.main()

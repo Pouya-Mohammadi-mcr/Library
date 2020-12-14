04b0abee27c988c487d4bce11f822e4fc81b8c22
Rujiravee Kongdee <rujiravee.kongdee@postgrad.manchester.ac.uk>
2020-12-14 22:11:36 +0700

    Increase the coverage percentage

--- test/test_database.py
+++ test/test_database.py
@@ -14,6 +14,7 @@ class TestDatabase(unittest.TestCase):
         db = database.Database()
         self.assertTrue(db.read(path.join(self.data_dir, "simple.xml")))
         self.assertEqual(len(db.publications), 1)
+        
 
     def test_read_invalid_xml(self):
         db = database.Database()
@@ -126,12 +127,86 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(data[0][0], 9999, "incorrect year in result")
         self.assertEqual(data[0][1], 2, "incorrect number of authors in result")
 
-    def test_get_author_firstlast(self):
+    def test_get_coauthor_data(self):
         db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "three-authors-and-three-publications.xml")))
-        header, data = db.get_author_firstlast()
-        self.assertEqual(data, [['author1', 1, 1], ['author2', 0, 1], ['author3', 0, 1]])
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "three-authors-and-three-publications.xml")))
+        header, data = db.get_coauthor_data(None,None,4)
+        self.assertEqual(len(data), 2,"incorrect number of rows")
+        self.assertEqual(data[0][0],"author1 1")
+        self.assertEqual(data[0][1],"author2 1")
+        self.assertEqual(data[1][0],"author2 1")
+        self.assertEqual(data[1][1],"author1 1")
+
+    def test_get_publication_summary_average(self):
+        MEANAPP=['Mean authors per publication', 2.0, 0, 1.0, 0, 1.75]
+        MEANPPA=['Mean publications per author', 1.5, 0.0, 0.25, 0.0, 1.75]
+        MEDAPP=['Median authors per publication', 2, 0, 1, 0, 1.5]
+        MEDPPA=['Median publications per author', 1.5, 0.0, 0.0, 0.0, 1.5]
+        MODEAPP=['Mode authors per publication', [1, 2, 3], [], [1], [], [1]]
+        MODEPPA=['Mode publications per author', [0.0, 1.0, 2.0, 3.0], [0.0], [0.0], [0.0], [1.0]]
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "sprint-2-acceptance-2.xml")))
+        _, data = db.get_publication_summary_average(database.Stat.MEAN)
+        self.assertEqual(data[0],MEANAPP)
+        self.assertEqual(data[1],MEANPPA)
+        _, data = db.get_publication_summary_average(database.Stat.MEDIAN)
+        self.assertEqual(data[0], MEDAPP)
+        self.assertEqual(data[1], MEDPPA)
+        _, data = db.get_publication_summary_average(database.Stat.MODE)
+        self.assertEqual(data[0], MODEAPP)
+        self.assertEqual(data[1], MODEPPA)
+
+    def test_get_average_authors_per_publication_by_year(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "sprint-2-acceptance-2.xml")))
+        header, data = db.get_average_authors_per_publication_by_year(database.Stat.MEAN)
+        self.assertEqual(len(header), len(data[0]), "header and data column size doesn't match")
+        self.assertEqual(len(data), 1, "incorrect number of years")
+        self.assertEqual(data[0][1], 2.0, "incorrect mean authors per conference paper for year1")
+        self.assertEqual(data[0][2], 0, "incorrect mean authors per journal for year1")
+        self.assertEqual(data[0][3], 1.0, "incorrect mean authors per book for year1")
+        self.assertEqual(data[0][4], 0, "incorrect mean authors per book chapter for year1")
+        self.assertEqual(data[0][5], 1.75, "incorrect mean authors per publication for year1")
 
 
+    def test_get_collaborations(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "sprint-2-acceptance-2.xml")))
+        data = db._get_collaborations(0,1)
+        self.assertEqual(data[0],3,"wrong number of publications")
+        self.assertEqual(data[1],1,"wrong count of collaboration with author3")
+        self.assertEqual(data[2],2,"wrong count of collaboration with author4") 
+
+    def test_get_coauthor_details(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "sprint-2-acceptance-2.xml")))
+        data = db.get_coauthor_details("AUTHOR1")
+        self.assertEqual(data[0][0],"AUTHOR1","wrong name for author")
+        self.assertEqual(data[0][1],3,"wrong number of publications")
+        self.assertEqual(data[1][0],"AUTHOR3","wrong name for coauthor")
+        self.assertEqual(data[1][1],1,"wrong count of collaboration with author3")
+        self.assertEqual(data[2][0],"AUTHOR4","wrong name for author")
+        self.assertEqual(data[2][1],2,"wrong count of collaboration with author4")
+
+    def test_all_authors(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "sprint-2-acceptance-2.xml")))  
+        print(db.get_all_authors())           
+    def test_get_network_data(self):
+        nodes = [['AUTHOR1', 2], ['AUTHOR3', 2], ['AUTHOR4', 2], ['AUTHOR2', 0]]
+        links = {(0, 1), (0, 2), (1, 2)}
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir,
+                                          "sprint-2-acceptance-2.xml")))
+        data = db.get_network_data()
+        self.assertEqual(data[0][0][0],"AUTHOR1","wrong name for author")   
+        self.assertEqual(data[0][0][1],2,"wrong number of authors in AUTHOR1's network")
+        self.assertEqual(data[0], nodes, "wrong nodes details")   
+        self.assertEqual(data[1], links, "wrong links details")   
 if __name__ == '__main__':
     unittest.main()

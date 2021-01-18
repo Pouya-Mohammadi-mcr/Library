2dba578e3665a08a283933c4f0cf064c551d031c
Rui Ma <rui.ma-6@postgrad.manchester.ac.uk>
2021-01-18 22:47:06 +0000

    Add click function to coauthors page.

--- src/comp62521/views.py
+++ src/comp62521/views.py
@@ -229,6 +229,27 @@ def showAuthorSearchByClick():
 
     return render_template("author_stats_by_click.html", args=args)
 
+@app.route("/authorstatsbyclick2/<string:author_name>")
+def showAuthorSearchByClick2(author_name):
+    result = ''.join([i for i in author_name if not i.isdigit()]).rstrip()
+    dataset=app.config['DATASET']
+    db = app.config['DATABASE']
+    args = {"dataset":dataset, "id":"author_stats_by_click"}
+    args["title"] = "Author Stats"
+    author = str(request.args.get("author"))
+    author_found, NoPublications, NoFirstAuthor, NoLastAuthor, NoSoleAuthor, NoCoAuthor, AuthorType, ExCoAuthorsList , NoExCoAuthors , author_name = db.get_author_stats_by_click(str(result))
+    args["authorfound"]=author_found
+    args["author"]=author
+    args["NoPublications"] = NoPublications
+    args["NoFirstAuthor"] = NoFirstAuthor
+    args["NoLastAuthor"] = NoLastAuthor
+    args["NoSoleAuthor"] = NoSoleAuthor
+    args["NoCoAuthor"] = NoCoAuthor
+    args["authorname"] = author_name
+    args["AuthorType"] = AuthorType
+    args["ExCoAuthorsList"] = ExCoAuthorsList
+    args["NoExCoAuthors"] = NoExCoAuthors
+    return render_template("author_stats_by_click.html", args=args)
 
 @app.route("/department_VS_authors")
 def showDepartment_VS_authors():
